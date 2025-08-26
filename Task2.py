89a5bd82f19d91d17b856ea34b2b35ca2f4ee192
CodeWithAnurag-in <natekaranurag06@gmail.com>
2025-08-26 15:23:16 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,12 @@
+import math
+
+user_input = int(input("Enter a number: "))
+
+square_root = math.sqrt(user_input)
+log = math.log(user_input)
+sine = math.sin(user_input)
+
+print("Square root: ", square_root)
+print("Log: ", log)
+print("Sine: ", sine)
+
