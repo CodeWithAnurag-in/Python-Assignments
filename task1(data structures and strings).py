f33883ce39a02de0f0acc6b01a3d997a9d12b8c8
CodeWithAnurag-in <natekaranurag06@gmail.com>
2025-08-27 14:19:36 +0530

    Add files via upload

---
+++ task1(data structures and strings).py
@@ -0,0 +1,7 @@
+username = input("Enter student's name: ")
+
+dict = {
+    "marks" : 85,
+}
+
+print(username +"'s", "marks is: ", dict["marks"])
---
+++ task2(data structures and strings).py
@@ -0,0 +1,7 @@
+l1 = [ 1 ,2 ,3 ,4 ,5 ,6 ,7, 8, 9, 10]
+extract = l1[0:5]
+print("Original list : ", l1)
+print("Extracted first five elements : ", extract)
+extract.reverse()
+reversed = extract
+print("Reverse extracted elements : ", reversed)
